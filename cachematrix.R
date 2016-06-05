58382b51697a96cc316e7e99dc6cf77a1c893d23
vishalj88 <vishaljada@gmail.com>
2016-06-05 18:35:12 -0400

    Addded function definitions for makecacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_matrix <-NULL
+  get <- function() x
+  set_inv_matrix <- function(matrix) inv_matrix <<-matrix
+  get_inv_matrix <- function() inv_matrix
+  
+  list(get = get, set_inv_matrix = set_inv_matrix, get_inv_matrix = get_inv_matrix)
 }
 
 
@@ -12,4 +17,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$get_inv_matrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  
+  data<- x$get()
+  m <- solve(data, ...)
+  x$set_inv_matrix(m)
+  m
 }
